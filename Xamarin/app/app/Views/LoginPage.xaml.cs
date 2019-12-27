322dc367cabd1574101c0715addc2c5f4b34ff2a
Bruno Ivan Zárate Fernández <brunoivanzaratefernandez@gmail.com>
2019-12-27 19:15:04 -0400

    Xamarin con conexion

--- Xamarin/app/app/Views/LoginPage.xaml.cs
+++ Xamarin/app/app/Views/LoginPage.xaml.cs
@@ -1,20 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace app.Views
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
+    [XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class LoginPage : ContentPage
 	{
-		public LoginPage ()
-		{
-			InitializeComponent ();
-		}
-	}
+        public LoginPage()
+        {
+            InitializeComponent();
+            btLogin.Clicked += BtLogin_Clicked;
+        }
+        private void BtLogin_Clicked(object sender, EventArgs e)
+        {
+            ((NavigationPage)this.Parent).PushAsync(new ListViewPage1 ());
+        }
+    }
 }
